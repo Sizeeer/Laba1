4b5f683987a414166400ff9ef61297c06cdbf407
Maksim Krisanov <igrekzero@gmail.com>
2022-09-06 05:30:18 +0300

    Initial commit

---
+++ app/src/main/java/krisanov/fit/bstu/by/MainActivity.java
@@ -0,0 +1,15 @@
+package krisanov.fit.bstu.by;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO: made app
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
